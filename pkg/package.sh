b22bb12765d8d10899a029239e646963771429de
PrabhjotJassalOrion <prabhjot.jassal@orionhealth.com>
2015-05-28 16:05:20 +1200

    Updated package.sh
    Add steps to create a directory with EAR file that is packaged into an RPM

--- pkg/package.sh
+++ pkg/package.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+mkdir connect
+
+cp ../Product/Production/Deploy/ear/glassfish/target/CONNECT-GF.ear connect/
+
 fpm \
   --rpm-os linux -a all \
   --pre-install pre-install.sh \
   -s dir -t rpm \
   --rpm-user orionConnect --rpm-group orionConnect \
-  --prefix /opt/orionConnect \
-  --description "Orion Connect" -n "orionConnect" \
+  --prefix /opt \
+  --description "Connect" -n "connect" \
   --version 0.0.1 \
   --iteration 1 \
   --directories . \
-  orionConnect-0.0.1
+  ./connect
+
+rm -rf connect
